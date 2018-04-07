9a6b75fef61421acb0ee8e8c9e4aaa7ce7af2989
Tom <shadowplaycoding@gmail.com>
2018-04-07 18:09:57 +1000

    Part One - Set up basic world and character movement

---
+++ JRPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move()
+	{
+		
+
+		if(Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+		{
+			float distance = Speed * Time.deltaTime;
+			float z = Input.GetAxis("Vertical") * distance;
+			float x = Input.GetAxis("Horizontal") * distance;
+
+			transform.Translate(x, 0, z);
+		}
+
+	}
+}
